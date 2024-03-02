c1d649cf815629eb7ef657e29014e02ae9523259
YeungLaiChing <yeunglaiching@gmail.com>
2024-03-02 14:14:01 +0800

    Update redis_hash_put.py

--- Python/Test/redis_hash_put.py
+++ Python/Test/redis_hash_put.py
@@ -5,7 +5,7 @@ pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
 r = redis.Redis(connection_pool=pool)
 
 r.hset("0005.HK", "price", "60.5")
-r.hset("0005.HK", "turnover", "3746838")
+r.hset("0005.HK", "turnover", 3746838)
 print(r.hkeys("0005.HK")) 
 print(r.hget("0005.HK", "price"))
 print(r.hmget("0005.HK", "price", "turnover"))
