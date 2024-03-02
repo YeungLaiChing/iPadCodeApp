08079623cdb9e9f7ffa56145ab341ba7ee110dfa
YeungLaiChing <yeunglaiching@gmail.com>
2024-03-02 14:11:15 +0800

    Update redis_hash_put.py

--- Python/Test/redis_hash_put.py
+++ Python/Test/redis_hash_put.py
@@ -6,10 +6,10 @@ r = redis.Redis(connection_pool=pool)
 
 r.hset("0005.HK", "price", "60.5")
 r.hset("0005.HK", "turnover", "3746838")
-print(r.hkeys("0005.HK")) # 取hash中所有的key
-print(r.hget("0005.HK", "price"))    # 单个取hash的key对应的值
-print(r.hmget("0005.HK", "price", "turnover")) # 多个取hash的key对应的值
-r.hsetnx("0005.HK", "price", "70") # 只能新建
+print(r.hkeys("0005.HK")) 
+print(r.hget("0005.HK", "price"))    
+print(r.hmget("0005.HK", "price", "turnover"))
+r.hsetnx("0005.HK", "price", "70")
 print(r.hget("0005.HK", "price"))
 r.hset("0005.HK", "price","71")
 print(r.hget("0005.HK","price")
