38d3510ee0d03ef1f68808ecd45be4fa1af59266
Ching <yeunglaiching@gmail.com>
2024-03-05 22:30:30 +0800

    te

--- Python/Test/graph/app1.py
+++ Python/Test/graph/app1.py
@@ -33,4 +33,4 @@ def homepage():
 # Main Driver Function 
 if __name__ == '__main__':
 	# Run the application on the local development server ##
-	app.run(port=6000, debug=True, host="0.0.0.0")
+	app.run(port=5000, debug=True, host="0.0.0.0")
