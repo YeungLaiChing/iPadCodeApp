17b6a60f9f984b3a3007469e8a5cee13600c0779
YeungLaiChing <yeunglaiching@gmail.com>
2024-03-11 18:24:12 +0800

    Update redis_timer.py

--- Python/Test/redis_timer.py
+++ Python/Test/redis_timer.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     sched = BackgroundScheduler(timezone='UCT')
     
     trigger = CronTrigger(
-        year="*", month="*", day="*", hour="*", minute="*", second="*"
+        year="*", month="*", day="*", hour="*", minute="*", second="0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58"
     )
     sched.add_job(
         trigger_job,
