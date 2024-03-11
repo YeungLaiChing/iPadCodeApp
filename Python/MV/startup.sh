326cdee5399caefc5716e360a8077cf22828d542
YeungLaiChing <yeunglaiching@gmail.com>
2024-03-11 19:22:35 +0800

    Update startup.sh

--- Python/MV/startup.sh
+++ Python/MV/startup.sh
@@ -4,5 +4,14 @@ nohup python3 mv_calc.py >> ./log/calculation.log 2>> ./log/calculation.err.log 
 
 sleep 3
 
+nohup python3 mv_calc_fix_int.py >> ./log/calcfix.log 2>> calcfix.err.log &
+
+sleep 3
+
 nohup python3 mv_capture.py >> ./log/capture.log 2>> ./log/capture.err.log &
+
+sleep 3
+
+nohup python3 mv_trigger.py >> ./log/trigger.log 2>> ./log/trigger.err.log &
+
 
