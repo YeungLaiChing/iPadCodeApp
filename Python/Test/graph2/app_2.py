8d675529842237f54da91e031224fe2425487bcf
Ching <yeunglaiching@gmail.com>
2024-03-12 14:38:37 +0800

    test

---
+++ Python/Test/graph2/app_2.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+#
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+import os
+import sys
+import time
+import numpy as np
+import pandas as pd
+from flask import Flask
+from flask import render_template
+from flask import json
+from datetime import datetime
+from datetime import timedelta
+from collections import defaultdict
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+@app.route("/stream")
+def stream():
+    return render_template('streamingvisjs_2.html', **locals())
+
+    
+
+@app.route("/getStreamingData")
+def getStreamingData():
+    df=pd.read_json('/Users/yeunglaiching/Workspace/iPadCodeApp/Python/Test/graph2/test.json',orient="records")
+    #df.set_index('exchangeTime', inplace=True)
+    return json.dumps(df.to_json(orient='index'))
+
+
+app.run(port=5000, debug=True, host="0.0.0.0")
