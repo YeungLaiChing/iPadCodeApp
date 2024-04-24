b05c2aa0a056cbd0f3604f17f85ed1a905598c3f
Ching <yeunglaiching@gmail.com>
2024-04-24 23:43:28 +0800

    test

--- Python/VA Index/CCIX/capture/consol.py
+++ Python/VA Index/CCIX/capture/consol.py
@@ -4,7 +4,7 @@ import sys
 from datetime import date, timedelta
 import json
 
-exchange = ''
+exchange = 'coinbase'
 if len(sys.argv) > 1:
     exchange=sys.argv[1]
 ccix_from_data_channel=f'ccix_{exchange}_btc_data_channel'
@@ -35,4 +35,13 @@ while True:
             exit()
         if message['data']!=1 :
             payload=json.loads(message['data'])
+            price=float(payload["price"])
+            volume=float(payload["volume"])
+            hr=payload["timestamp_hrs"]
+            exchange=payload["exchange"]
+            acc_vol=volume;
+            if rds.hget(hr,exchange) :
+                acc_vol=volume+float(rds.hget(hr,exchange))
+            rds.hset(hr,exchange,acc_vol)
+            payload["acc_vol"]=acc_vol
             rds.publish(ccix_consol_data_channel,json.dumps(payload))
