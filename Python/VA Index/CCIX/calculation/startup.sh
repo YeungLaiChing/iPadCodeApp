7697592f5a736f365d3a4f203cf3166ad527e5c4
Ching <yeunglaiching@gmail.com>
2024-04-26 21:19:40 +0800

    test

--- Python/VA Index/CCIX/calculation/startup.sh
+++ Python/VA Index/CCIX/calculation/startup.sh
@@ -3,3 +3,4 @@ BASEDIR="$( cd "$( dirname "$0" )" && pwd )"
 mkdir "${BASEDIR}"/data/ "${BASEDIR}"/log/ 2>/dev/null >/dev/null
 cd "${BASEDIR}"
 
+python3 btc_index_tick.py >> ./log/calc_btc.log 2>> ./log/calc_btc.err &
