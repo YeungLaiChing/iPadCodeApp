07114b7461e84a68fb029ae662f96b423d097c9d
YeungLaiChing <yeunglaiching@gmail.com>
2024-05-04 11:53:10 +0800

    Update testdb.py

--- Python/Test/DynamoDB/testdb.py
+++ Python/Test/DynamoDB/testdb.py
@@ -10,22 +10,14 @@ def create_books_table(dynamodb=None):
             {
                 'AttributeName': 'book_id',
                 'KeyType': 'HASH'  # Partition key
-            },
-            {
-                'AttributeName': 'title',
-                'KeyType': 'RANGE'  # Sort key
-            }
+            
         ],
         AttributeDefinitions=[
             {
                 'AttributeName': 'book_id',
                 # AttributeType refers to the data type 'N' for number type and 'S' stands for string type.
                 'AttributeType': 'N'
-            },
-            {
-                'AttributeName': 'title',
-                'AttributeType': 'S'
-            },
+            }
         ],
         ProvisionedThroughput={
             # ReadCapacityUnits set to 10 strongly consistent reads per second
@@ -33,8 +25,8 @@ def create_books_table(dynamodb=None):
             'WriteCapacityUnits': 10  # WriteCapacityUnits set to 10 writes per second
         }
     )
-    return table
+    print(table.status)
 
 if __name__ == '__main__':
-    book_table = create_books_table()
-    print("Status:", book_table.table_status)
+    create_books_table()
+    
