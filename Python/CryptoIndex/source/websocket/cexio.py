e0d2a7ec44f340180da34abb5d9ab09d995e82c2
Ching <yeunglaiching@gmail.com>
2024-05-13 21:19:53 +0800

    te

--- Python/CryptoIndex/source/websocket/cexio.py
+++ Python/CryptoIndex/source/websocket/cexio.py
@@ -1,23 +1,43 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
 import json
 import csv
 import threading
 from datetime import datetime, timezone, timedelta
 from websocket import WebSocketApp
-import websocket
 import time
 import redis
+import os
+from pathlib import Path
 
-### PENDING
-rds = redis.Redis(host='redis-va', port=6379, db=0,decode_responses=True)
-ccix_data_channel='ccix_cexio_btc_data_channel'
-csv_file_path='./data/cexio_btc.csv'
+redis_host=os.environ.get('REDIS_HOST', '192.168.0.3')
+redis_port=int(os.environ.get('REDIS_PORT', '6379'))
+product_ids=os.environ.get('PROD_ID', "BTC-USD")
+crypto_asset=os.environ.get('CRYPTO_ASSET','BTC')
+exchange_name=os.environ.get('EXCHANGE_NAME','cexio')
 
+rds = redis.Redis(host=redis_host, port=redis_port, db=0,decode_responses=True)
+
+
+data_path="./data/"
+file_list={}
+def get_date_partition(input):
+    return f"{datetime.fromtimestamp(int(input+16*3600), tz=timezone.utc).strftime('%Y%m%d')}"
+               
+def get_path_by_time(now):
+    folder_partition=get_date_partition(now)
+    path=f"{data_path}/{folder_partition}"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    return path
+
+file_name=f'{exchange_name.lower()}_{crypto_asset.lower()}.csv'
+ccix_data_channel=f'ccix_{exchange_name.lower()}_{crypto_asset.lower()}_data_channel'
 
 lock=threading.Lock()
 def get_current_time():
     return datetime.fromtimestamp(int(time.time()+8*3600), tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
 
-def write_to_csv(data_row):
+def write_to_csv(csv_file_path,data_row):
     with lock:
         with open(csv_file_path,mode='a', newline='') as file:
             csv_writer = csv.writer(file)
@@ -27,10 +47,9 @@ def write_to_csv(data_row):
 def process_message(message):
     try:
         data = json.loads(message)
-        print(data)
-        print(data['e'])
+        
         if data['e']=='tradeUpdate':
-            print(data)
+           
             original_timestamp = data['data']['dateISO']
             utc_datetime = datetime.strptime(original_timestamp,'%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc)
             unix_timestamp=int(utc_datetime.timestamp())
@@ -61,7 +80,13 @@ def process_message(message):
                 'volume':last_quantity
             }
             rds.publish(ccix_data_channel,json.dumps(payload))
-            write_to_csv(data_row )
+            global file_list
+            partition=get_date_partition(unix_timestamp)
+            if partition not in file_list:
+                file_list[partition]=f"{get_path_by_time(unix_timestamp)}/{file_name}"
+                setup_csv_file(file_list[partition])
+                
+            write_to_csv(file_list[partition],data_row)
             print(f"saved data to csv: {data_row}")
     except json.JSONDecodeError as e:
         print(f"JSON decode error: {e}")
@@ -90,20 +115,29 @@ def on_open(ws):
         "e": "trade_subscribe",
         "oid": "72955210375_trade_subscribe",
         "data": {
-            "pair": "BTC-USD"
+            "pair": product_ids
         }
     })
     ws.send(subscribe_message)
     print("sent subscribe")
 
-def setup_csv_file():
+def setup_csv_file(csv_file_path):
     with open(csv_file_path,mode='a',newline='') as file:
         if file.tell() == 0:
             csv_writer=csv.writer(file)
             csv_writer.writerow(['timestamp','unit_ts','hkt','tradeid','price','quantity'])
             
+def trigger_job(ws):
+    #print('trigger_job in fixed interval')
+    pingPayload=json.dumps({"e":"ping"})
+    ws.send(pingPayload)
+    
+                
 def get_data():
-    setup_csv_file()
+    now=time.time()
+    global file_list
+    file_list[get_date_partition(now)]=f"{get_path_by_time(now)}/{file_name}"
+    setup_csv_file(file_list[get_date_partition(now)])
     ws=WebSocketApp("wss://trade.cex.io/api/spot/ws-public",
                     on_open=on_open,
                     on_message=on_message,
@@ -111,8 +145,24 @@ def get_data():
                     on_ping=on_ping,
                     on_pong=on_pong,
                     on_close=on_close)
-    pingPayload=json.dumps({"e":"pong"})
-    ws.run_forever(ping_interval=8, ping_timeout=5, ping_payload=pingPayload)
+    
+    
+    sched = BackgroundScheduler(timezone='UCT')
+    
+    trigger = CronTrigger(
+        year="*", month="*", day="*", hour="*", minute="*", second="0,5,10,15,20,25,30,35,40,45,50,55"
+    )
+    sched.add_job(
+        trigger_job,
+        trigger=trigger,
+        args=[ws],
+        name="trigger job",
+    )  
+    #sched.add_job(job2,'interval',id='2_sec',seconds=15)
+    time.sleep(5)
+    sched.start()
+    
+    ws.run_forever()
     
     
 if __name__ == "__main__":
