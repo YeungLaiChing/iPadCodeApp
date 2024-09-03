188b9d3af23b96ab0d2791ca99b987dcb370db7b
YeungLaiChing <yeunglaiching@gmail.com>
2024-09-03 10:52:50 +0800

    test

--- Python/Test/RZT/first.py
+++ Python/Test/RZT/first.py
@@ -1,5 +1,4 @@
 import requests
-session = requests.session()
 # login
 url = "https://tickdata.hkex.datahex.rozettatech.com/api/auth/login"
 
@@ -28,7 +27,7 @@ payloads = {
 #  }
 #}'
 
-r = session.post(url=url, headers=headers, data=payloads)
+r = requests.post(url=url, headers=headers, data=payloads)
 
 print("===== login resp text=========")
 print(r.text)
@@ -41,26 +40,6 @@ print(cook.get_dict())
 
 
 # get data
-url = "https://tickdata.hkex.datahex.rozettatech.com/api/data/v1/datasets/tick/security_types"
-headers = {
-    'Content-Tpye':'application/json',
-    'accept': 'application/json'
-}
+# url = "https://tickdata.hkex.datahex.rozettatech.com/api/data/v1/datasets/tick/security_types"
 
-r=session.get(url=url,headers=headers,cookies=cook)
-
-
-print("===== get resp text=========")
-print(r.text)
-print("===== get resp header=========")
-print(r.headers)
-print("===== get resp cookies=========")
-cook=r.cookies
-print(cook)
-print(cook.get_dict())
-
-session.cookies.clear()
-
-session.close()
-
-exit()
+# r=requests.get(url=url,cookies=cook)
