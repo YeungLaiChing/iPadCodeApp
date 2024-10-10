3323af1a0894e15516ccd1fcc31d6a103b61608b
YeungLaiChing <yeunglaiching@gmail.com>
2024-10-10 14:15:52 +0800

    test

--- Python/Test/YahooFinance/rest.py
+++ Python/Test/YahooFinance/rest.py
@@ -1,28 +1,57 @@
 import requests
 import json
-# login
-crumb = "https://query1.finance.yahoo.com/v1/test/getcrumb"
-
-#'Content-Tpye':'application/json',
-#'accept': 'application/json',
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:42.0) Gecko/20100101 Firefox/42.0'
-}
-
-r = requests.get(url=crumb,headers=headers)
-print("===== login resp text=========")
-print(r.text)
-print("===== login resp header=========")
-print(r.headers)
-print("===== login resp cookies=========")
-cookies=r.cookies
-print(cookies)
-
-crumb_number="ABC"
-#get data
-url = f"https://query1.finance.yahoo.com/v7/finance/quote?&symbols=BTC-USD&fields=currency,regularMarketChange,regularMarketChangePercent,regularMarketPrice&crumb={crumb_number}"
-#r=requests.get(url=url,cookies=cookies)
-#print("===== GET resp text=========")
-#print(r.text)
 
 
+import requests
+
+
+def get_yahoo_cookie():
+    cookie = None
+
+    user_agent_key = "User-Agent"
+    user_agent_value = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+
+    headers = {user_agent_key: user_agent_value}
+    response = requests.get(
+        "https://fc.yahoo.com", headers=headers, allow_redirects=True
+    )
+
+    if not response.cookies:
+        raise Exception("Failed to obtain Yahoo auth cookie.")
+
+    cookie = list(response.cookies)[0]
+
+    return cookie
+
+
+def get_yahoo_crumb(cookie):
+    crumb = None
+
+    user_agent_key = "User-Agent"
+    user_agent_value = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+
+    headers = {user_agent_key: user_agent_value}
+
+    crumb_response = requests.get(
+        "https://query1.finance.yahoo.com/v1/test/getcrumb",
+        headers=headers,
+        cookies={cookie.name: cookie.value},
+        allow_redirects=True,
+    )
+    crumb = crumb_response.text
+
+    if crumb is None:
+        raise Exception("Failed to retrieve Yahoo crumb.")
+
+    return crumb
+
+def get_yahoo_data(crumb_number):
+    url = f"https://query1.finance.yahoo.com/v7/finance/quote?&symbols=BTC-USD&fields=currency,regularMarketChange,regularMarketChangePercent,regularMarketPrice&crumb={crumb_number}"
+    r=requests.get(url=url,cookies=cookies)
+    print("===== GET resp text=========")
+    print(r.text)
+
+# Usage
+cookie = get_yahoo_cookie()
+crumb = get_yahoo_crumb(cookie)
+get_yahoo_data(crumb_number=crumb)
