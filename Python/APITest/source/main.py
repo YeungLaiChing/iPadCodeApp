605f7657e7757084f0907a466711c75af38ee6e5
YeungLaiChing <yeunglaiching@gmail.com>
2024-10-18 15:12:33 +0800

    test

--- Python/APITest/source/main.py
+++ Python/APITest/source/main.py
@@ -1,10 +1,13 @@
 import os
+from flask import Flask
+from flask import render_template
+from flask import json
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
 @app.route("/apiTestMain")
-def stream():
+def testmain():
     return render_template('main.html', **locals())
 
 app.run(port=5000, debug=True, host="0.0.0.0")
