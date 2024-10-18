d513193e044e58c1ed492090e545f6842d7b6e5a
YeungLaiChing <yeunglaiching@gmail.com>
2024-10-18 18:35:30 +0800

    test

--- Python/APITest/source/api_test.py
+++ Python/APITest/source/api_test.py
@@ -47,14 +47,25 @@ def login_token():
   
 @app.route('/apiTestPostJson', methods=['POST'])
 def api_test_json():
-    method="POST"
+    method=request.form.get('action_type')
     content_server=request.form.get('server')
     content_url=request.form.get('url')
-    #token=login_token()
     token=request.form.get('token')
     content_header={    'Content-Tpye':'application/json',    "Authorization": f"Bearer {token}"  }
-    content_data=json.loads(request.form.get('data'))
-    r=requests.post(url=f"{content_server}{content_url}",headers=content_header,json=content_data)
+    if method == "POST":
+        content_data=json.loads(request.form.get('data'))
+        r=requests.post(url=f"{content_server}{content_url}",headers=content_header,json=content_data)
+    if method == "GET":
+        r=requests.get(url=f"{content_server}{content_url}",headers=content_header)
+    if method == "PUT":
+        content_data=json.loads(request.form.get('data'))
+        r=requests.put(url=f"{content_server}{content_url}",headers=content_header,json=content_data)
+    if method == "DELETE":
+        r=requests.delete(url=f"{content_server}{content_url}",headers=content_header)
+    if method == "patch":
+        content_data=json.loads(request.form.get('data'))
+        r=requests.get(url=f"{content_server}{content_url}",headers=content_header,json=content_data)
+
     print(r.status_code)
     print(r.text)    
     return r.text
